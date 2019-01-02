bbdd6568930e99d4ca636697f48a6ce4f53cb19a
Chris Marshall <rookiegamedev@gmail.com>
2019-01-02 19:36:25 +0000

    Refactored code

--- lanes.py
+++ lanes.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 raw_image = cv2.imread('Road_Test.jpg')
 
+def canny(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
+    return cv2.Canny(blur, 50, 150)
+
 def make_coordinates(image, line_params):
     slope, intercept = line_params
     y1 = image.shape[0]
@@ -9,37 +14,6 @@ def make_coordinates(image, line_params):
     x1 = int((y1 - intercept)/slope)
     x2 = int((y2 - intercept)/slope)
     return np.array([x1, y1, x2, y2])
-
-def average_slope(image, lines):
-    left_lines = []
-    right_lines = []
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        paramaters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = paramaters[0]
-        intercept = paramaters[1]
-        if slope < 0:
-            left_lines.append((slope, intercept))
-        else:
-            right_lines.append((slope, intercept))
-    left_lines_avg = np.average(left_lines, axis = 0)
-    right_lines_avg = np.average(right_lines, axis = 0)
-    make_left_line = make_coordinates(image, left_lines_avg)
-    make_right_line = make_coordinates(image, right_lines_avg)
-    return np.array([make_left_line, make_right_line])
-        
-def canny(image):
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
-    return cv2.Canny(blur, 50, 150)
-
-def display_lines(image, lines):
-    line_image = np.zeros_like(image)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-    return line_image
 
 def region_of_interest(image):
     height = image.shape[0]
@@ -49,6 +23,29 @@ def region_of_interest(image):
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     return cv2.bitwise_and(image, mask)
+
+def average_slope(image, lines):
+    left_lines, right_lines = [], []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        paramaters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = paramaters[0]
+        intercept = paramaters[1]
+        if slope < 0:
+            left_lines.append((slope, intercept))
+        else:
+            right_lines.append((slope, intercept))
+    return np.array([
+        make_coordinates(image, np.average(left_lines, axis = 0)),
+        make_coordinates(image, np.average(right_lines, axis = 0))
+    ])
+        
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
 
 raw_copy = np.copy(raw_image)
 cropped_image = region_of_interest(canny(raw_copy))
