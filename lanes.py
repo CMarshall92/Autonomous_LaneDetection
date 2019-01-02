74fdeab28edb1fad66715b5c28feb30462e5dc5a
Chris Marshall <rookiegamedev@gmail.com>
2019-01-02 17:10:43 +0000

    added copy and converted to greyscale

--- lanes.py
+++ lanes.py
@@ -1,5 +1,15 @@
 import cv2
+import numpy as np
 
+# Loads in the images using the cv2 lib
 image = cv2.imread('Road_Test.jpg')
-cv2.imshow('result', image)
+
+# Creates a copy of the image to be later converted to grayscale
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+# Displays the image onscreen in a new window titled result
+cv2.imshow('result', gray)
 cv2.waitKey(0)
+
+
